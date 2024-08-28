ac19a5c8003d533285b552ae448fcbd4fd3ac47d
Diogo Kawaguchi Rosa <diogo.kawaguchi.rosa@gmail.com>
2024-08-28 14:11:09 -0300

    :construction: wip: Adicionado condição para diferenciar números de operação

--- js/script.js
+++ js/script.js
@@ -9,6 +9,11 @@ class Calculator{
 buttons.forEach((btn) => {
     btn.addEventListener("click", (e)=>{
         const value = e.target.innerText
-        console.log(value)
+        
+        if(+value >= 0 || value === "."){
+            console.log(value)
+        }else{
+            console.log("Op "+value)
+        }
     })
 })
