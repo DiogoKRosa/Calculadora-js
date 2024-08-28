d6c64824da8ba306883c9eb806834587c8a3bd37
Diogo Kawaguchi Rosa <diogo.kawaguchi.rosa@gmail.com>
2024-08-28 14:36:40 -0300

    :construction: wip: Lendo as operações matemáticas e passando para o visor

--- js/script.js
+++ js/script.js
@@ -2,35 +2,102 @@ const previousOperationText = document.querySelector('#previous-operation')
 const currentOperationText = document.querySelector('#current-operation')
 const buttons = document.querySelectorAll('#buttons-container button')
 
-class Calculator{
-    constructor(previousOperationText, currentOperationText){
+class Calculator {
+    constructor(previousOperationText, currentOperationText) {
         this.previousOperationText = previousOperationText
         this.currentOperationText = currentOperationText
         this.currentOperation = ""
     }
 
     // add digit to calculator screen
-    addDigit(digit){
+    addDigit(digit) {
+        // check if current operation already has a dot
+        if (digit === "." && this.currentOperationText.innerText.includes(".")) {
+            return
+        }
         this.currentOperation = digit
         this.updateScreen()
     }
 
+    //Process all calculator operations
+    processOperation(operation) {
+        // Check if current is empty
+        if(this.currentOperationText.innerText === ""){
+            // Change operation
+            if(this.previousOperationText.innerText !== ""){
+                this.changeOperation(operation)
+            }
+            return
+        }
+
+        // Get current and previous value
+        let operationValue
+        const previous = +this.previousOperationText.innerText.split(" ")[0]
+        const current = +this.currentOperationText.innerText
+
+        switch (operation) {
+            case "+":
+                operationValue = previous + current
+                this.updateScreen(operationValue, operation, current, previous)
+                break
+            case "-":
+                operationValue = previous - current
+                this.updateScreen(operationValue, operation, current, previous)
+                break
+            case "*":
+                operationValue = previous * current
+                this.updateScreen(operationValue, operation, current, previous)
+                break    
+            case "/":
+                operationValue = previous / current
+                this.updateScreen(operationValue, operation, current, previous)
+                break   
+            default:
+                return
+        }
+    }
+
     // Change values of the calculator screen
-    updateScreen(){
-        this.currentOperationText.innerText += this.currentOperation
+    updateScreen(
+        operationValue = null, 
+        operation = null, 
+        current = null, 
+        previous = null
+    ) {
+        if(operationValue === null){
+            this.currentOperationText.innerText += this.currentOperation
+        }else{
+            // Check if value is zero, if it is just add current value
+            if(previous === 0){
+                operationValue = current
+            }
+
+            // Add current value to previous
+            this.previousOperationText.innerText = `${operationValue} ${operation}`
+            this.currentOperationText.innerText = ""
+        }
+    }
+
+    // Change math operation
+    changeOperation(operation){
+        const mathOperation = ["*", "+", "-", "/"]
+        if(!mathOperation.includes(operation)){
+            return
+        }
+        this.previousOperationText.innerText = this.previousOperationText.innerText.slice(0, -1) + operation
     }
 }
 
 const calc = new Calculator(previousOperationText, currentOperationText)
 
 buttons.forEach((btn) => {
-    btn.addEventListener("click", (e)=>{
+    btn.addEventListener("click", (e) => {
         const value = e.target.innerText
-        
-        if(+value >= 0 || value === "."){
+
+        if (+value >= 0 || value === ".") {
             calc.addDigit(value)
-        }else{
-            console.log("Op "+value)
+        } else {
+            calc.processOperation(value)
         }
     })
 })
